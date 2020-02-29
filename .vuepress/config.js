5e414ee5c97a8c3a5624e84ccc9b77dea144d346
吴贻韡 <mizuka.wu@outlook.com>
2020-02-29 16:25:23 +0800

    feat: 添加说明文字

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -10,21 +10,25 @@ module.exports = {
     sidebar: "auto",
     nav: [
       {
-        text: "分类",
+        text: "按人物查看",
         items: nav
       },
       {
-        text: "源",
+        text: "切换访问源",
         items: [
           {
             text: "github源",
             link: "https://www.mizuka.top/vocaloid-module-index/"
           },
           {
-            text: "gitee源",
+            text: "gitee源(国内会快)",
             link: "https://mizuka.gitee.io/vocaloid-module-index/"
           }
         ]
+      },
+      {
+        text: "github",
+        link: "https://github.com/mizuka-wu/vocaloid-module-index"
       }
     ]
   }
