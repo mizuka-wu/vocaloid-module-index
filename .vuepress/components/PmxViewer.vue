0f9fda79c3b50192f61c69639fa7a8c5279be996
吴贻韡 <mizuka.wu@outlook.com>
2020-03-01 15:20:34 +0800

    feat: pmx-viewer增加按钮

--- .vuepress/components/PmxViewer.vue
+++ .vuepress/components/PmxViewer.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
+  <div :class="{ fullscreen, pmxviewer: true }">
+    <button class="fullscreen-button" @click="fullscreen = !fullscreen">
+      点击{{ fullscreen ? "取消" : "" }}全屏
+    </button>
     <iframe
       style="border: 0"
       :src="$withBase(src)"
@@ -16,6 +19,11 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      fullscreen: false
+    };
+  },
   computed: {
     src() {
       const { path } = this;
@@ -24,3 +32,22 @@ export default {
   }
 };
 </script>
+<style scoped>
+.fullscreen {
+  position: fixed !important;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 9999;
+}
+.fullscreen-button {
+  position: absolute;
+  right: 0;
+  top: 0;
+  z-index: 10000;
+}
+.pmxviewer {
+  position: relative;
+}
+</style>
